529b3c9a51ad9da106b7e610f95bff8f8a91c69a
RoseSecurity <michael@rosesecurity.dev>
2026-03-17 11:11:47 -0400

    chore: clean up tests

--- test/component_test.go
+++ test/component_test.go
@@ -10,8 +10,8 @@ import (
 	"github.com/cloudposse/test-helpers/pkg/atmos"
 	helper "github.com/cloudposse/test-helpers/pkg/atmos/component-helper"
 	awshelper "github.com/cloudposse/test-helpers/pkg/aws"
-	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -42,21 +42,21 @@ func (s *ComponentSuite) TestAcm() {
 
 	assert.NotNil(s.T(), options)
 
-	alb_name := atmos.Output(s.T(), options, "alb_name")
-	alb_arn := atmos.Output(s.T(), options, "alb_arn")
-	alb_arn_suffix := atmos.Output(s.T(), options, "alb_arn_suffix")
+	albName := atmos.Output(s.T(), options, "alb_name")
+	albARN := atmos.Output(s.T(), options, "alb_arn")
+	albARNSuffix := atmos.Output(s.T(), options, "alb_arn_suffix")
 
-	assert.True(s.T(), strings.HasPrefix(alb_arn_suffix, fmt.Sprintf("app/%s", alb_name)))
+	assert.True(s.T(), strings.HasPrefix(albARNSuffix, fmt.Sprintf("app/%s", albName)))
 
-	awsAccountId := aws.GetAccountId(s.T())
+	awsAccountID := aws.GetAccountId(s.T())
 
-	expectedArn := fmt.Sprintf("arn:aws:elasticloadbalancing:%s:%s:loadbalancer/%s", awsRegion, awsAccountId, alb_arn_suffix)
-	assert.Equal(s.T(), expectedArn, alb_arn)
+	expectedArn := fmt.Sprintf("arn:aws:elasticloadbalancing:%s:%s:loadbalancer/%s", awsRegion, awsAccountID, albARNSuffix)
+	assert.Equal(s.T(), expectedArn, albARN)
 
 	client := awshelper.NewElbV2Client(s.T(), awsRegion)
 
 	loadBalancers, err := client.DescribeLoadBalancers(context.Background(), &elasticloadbalancingv2.DescribeLoadBalancersInput{
-		LoadBalancerArns: []string{alb_arn},
+		LoadBalancerArns: []string{albARN},
 	})
 	assert.NoError(s.T(), err)
 
@@ -64,17 +64,17 @@ func (s *ComponentSuite) TestAcm() {
 		s.T().Fatal("No load balancers found")
 	}
 	loadBalancer := loadBalancers.LoadBalancers[0]
-	alb_dns_name := atmos.Output(s.T(), options, "alb_dns_name")
-	assert.Equal(s.T(), *loadBalancer.DNSName, alb_dns_name)
+	albDNSName := atmos.Output(s.T(), options, "alb_dns_name")
+	assert.Equal(s.T(), *loadBalancer.DNSName, albDNSName)
 
-	alb_zone_id := atmos.Output(s.T(), options, "alb_zone_id")
-	assert.Equal(s.T(), *loadBalancer.CanonicalHostedZoneId, alb_zone_id)
+	albZoneID := atmos.Output(s.T(), options, "alb_zone_id")
+	assert.Equal(s.T(), *loadBalancer.CanonicalHostedZoneId, albZoneID)
 
-	security_group_id := atmos.Output(s.T(), options, "security_group_id")
-	assert.Equal(s.T(), loadBalancer.SecurityGroups[0], security_group_id)
+	securityGroupID := atmos.Output(s.T(), options, "security_group_id")
+	assert.Equal(s.T(), loadBalancer.SecurityGroups[0], securityGroupID)
 
 	targetGroups, err := client.DescribeTargetGroups(context.Background(), &elasticloadbalancingv2.DescribeTargetGroupsInput{
-		LoadBalancerArn: &alb_arn,
+		LoadBalancerArn: &albARN,
 	})
 	assert.NoError(s.T(), err)
 
@@ -82,38 +82,36 @@ func (s *ComponentSuite) TestAcm() {
 		s.T().Fatal("No target groups found")
 	}
 	targetGroup := targetGroups.TargetGroups[0]
-	default_target_group_arn := atmos.Output(s.T(), options, "default_target_group_arn")
-	assert.Equal(s.T(), *targetGroup.TargetGroupArn, default_target_group_arn)
+	defaultTargetGroupARN := atmos.Output(s.T(), options, "default_target_group_arn")
+	assert.Equal(s.T(), *targetGroup.TargetGroupArn, defaultTargetGroupARN)
 
-	listener_arns := atmos.OutputList(s.T(), options, "listener_arns")
-	assert.Equal(s.T(), 2, len(listener_arns))
+	listenerARNS := atmos.OutputList(s.T(), options, "listener_arns")
+	assert.Equal(s.T(), 2, len(listenerARNS))
 
-	listeners, err := client.DescribeListeners(context.Background(), &elasticloadbalancingv2.DescribeListenersInput{LoadBalancerArn: &alb_arn})
+	listeners, err := client.DescribeListeners(context.Background(), &elasticloadbalancingv2.DescribeListenersInput{LoadBalancerArn: &albARN})
 	assert.NoError(s.T(), err)
 
 	assert.Equal(s.T(), 2, len(listeners.Listeners))
 
-	http_redirect_listener_arn := atmos.Output(s.T(), options, "http_redirect_listener_arn")
-	https_listener_arn := atmos.Output(s.T(), options, "https_listener_arn")
+	httpRedirectListenerARN := atmos.Output(s.T(), options, "http_redirect_listener_arn")
+	httpsListenerARN := atmos.Output(s.T(), options, "https_listener_arn")
 
 	for _, listener := range listeners.Listeners {
 		if *listener.Port == 443 {
-			assert.Equal(s.T(), *listener.ListenerArn, https_listener_arn)
+			assert.Equal(s.T(), *listener.ListenerArn, httpsListenerARN)
 			assert.EqualValues(s.T(), "HTTPS", listener.Protocol)
 		} else {
-			assert.Equal(s.T(), *listener.ListenerArn, http_redirect_listener_arn)
+			assert.Equal(s.T(), *listener.ListenerArn, httpRedirectListenerARN)
 			assert.EqualValues(s.T(), "HTTP", listener.Protocol)
 		}
-		assert.Contains(s.T(), listener_arns, *listener.ListenerArn)
+		assert.Contains(s.T(), listenerARNS, *listener.ListenerArn)
 	}
 
-	access_logs_bucket_id := atmos.Output(s.T(), options, "access_logs_bucket_id")
-	assert.Equal(s.T(), "", access_logs_bucket_id)
+	accessLogsBucketID := atmos.Output(s.T(), options, "access_logs_bucket_id")
+	assert.Equal(s.T(), "", accessLogsBucketID)
 
 	s.DriftTest(component, stack, nil)
-
 }
-
 
 func (s *ComponentSuite) TestEnabledFlag() {
 	const component = "alb/disabled"
@@ -127,14 +125,15 @@ func TestRunSuite(t *testing.T) {
 	suite.AddDependency(t, "vpc", "default-test", nil)
 
 	subdomain := strings.ToLower(random.UniqueId())
-	inputs := map[string]interface{}{
-		"zone_config": []map[string]interface{}{
+	inputs := map[string]any{
+		"zone_config": []map[string]any{
 			{
 				"subdomain": subdomain,
 				"zone_name": "components.cptest.test-automation.app",
 			},
 		},
 	}
+	suite.AddDependency(t, "dns-primary", "default-test", &inputs)
 	suite.AddDependency(t, "dns-delegated", "default-test", &inputs)
 	suite.AddDependency(t, "acm", "default-test", nil)
 	helper.Run(t, suite)
